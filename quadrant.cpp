6f1a9117beb06e38f59d0528d669d5df7fc3a33a
ufkw <42676735+ufkw@users.noreply.github.com>
2021-01-08 14:27:20 -0500

    Create quadrant.cpp
    "Quadrant" solution

---
+++ quadrant.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int x,y;
+    cin >> x;
+    cin >> y;
+
+    //check quadrant
+
+    if(x > 0)
+    {
+        if(y > 0)
+            cout << "1";
+        else
+            cout << "4";
+    }
+    else
+    {
+        if(y > 0)
+            cout << "2";
+        else
+            cout << "3";
+    }
+
+}
